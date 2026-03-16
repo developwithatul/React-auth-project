882aaa5ec869639bf3ec1dc010ccbffcb716049a
atul Chakrawarti <atul.chakra.tech@gmail.com>
2026-03-16 21:27:34 +0530

    Update Auth.jsx

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -81,11 +81,11 @@ const Auth = () => {
               {/* social button */}
               <div className=" grid grid-cols-2 gap-6 mt-6">
                 <button className="flex items-center justify-center border gap-2 py-2 border-gray-300">
-                  <img className="w-7" src="src/assets/google.png" alt="" />
-                  Googl
+                  <img className="w-7" src="src/assets/google.png" alt="img_google" />
+                  Google
                 </button>
                 <button className="flex items-center justify-center border gap-2 py-2 border-gray-300">
-                  <img className="w-7" src="src/assets/facebook.avif" alt="" />
+                  <img className="w-7" src="src/assets/facebook.avif" alt="img_google" />
                   Facebook
                 </button>
               </div>
