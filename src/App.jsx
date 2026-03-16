0cfd981503d5bebe4bf3bab7fe40a1f0b470d067
atulchakrawarti <atul.chakra.tech@gmail.com>
2026-03-16 20:40:01 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
       <Routes>
          <Route path='/' element={<Auth />} />
          <Route path="/auth" element={<Auth />} />
-         <Route path="*" element={<Auth />} />
       </Routes>
  </div>
   )
